bfc3c9bde9ad8b94d80683ca8c2fb4139e0ef137
Hadong Lee <hadongme@gmail.com>
2023-02-05 20:39:44 +0900

    init project with tailwindcss

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vue Sandbox" />
+  <div class="justify-center flex bg-gradient-to-b from-gray-900 to-gray-600 items-center h-screen">
+    <div class="text-4xl text-white">
+      Start New Project!
+    </div>
+  </div>
 </template>
 
 <style scoped>
