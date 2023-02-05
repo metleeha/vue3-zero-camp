7d07c81b5ca22a3d65bf400ace67536bb815b68f
Hadong Lee <hadongme@gmail.com>
2023-02-05 21:33:43 +0900

    add forms

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
+import SignupForm from './components/SignupForm.vue';
 </script>
 
 <template>
-  <div class="justify-center flex bg-gradient-to-b from-gray-900 to-gray-600 items-center h-screen">
-    <div class="text-4xl text-white">
-      Start New Project!
-    </div>
+  <div>
+    <SignupForm />
   </div>
 </template>
 
