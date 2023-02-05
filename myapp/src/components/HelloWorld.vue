9aedb4fc8932f49bda5b53d4a7a7055e9c27cfc9
Hadong Lee <hadongme@gmail.com>
2023-02-05 15:23:23 +0900

    vue sandbox

---
+++ myapp/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+
+  <div class="title">
+    <h1>{{ msg }}</h1>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
