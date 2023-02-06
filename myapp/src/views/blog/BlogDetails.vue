e2674fa0aa33ca4f033c58154d99cd26b0b8c552
Hadong Lee <hadongme@gmail.com>
2023-02-07 01:02:39 +0900

    Fetch data using Json Server

--- myapp/src/views/blog/BlogDetails.vue
+++ myapp/src/views/blog/BlogDetails.vue
@@ -1,18 +1,28 @@
 
 <script>
-    export default {
-        name: 'BlogDetails',
-        data() {
-            return {
-                id: this.$route.params.id
-            }
+export default {
+    props: ['id'],
+    data() {
+        return {
+            blog: null
         }
-    }
+    },
+    mounted() {
+        fetch('http://localhost:3000/blogs/' + this.id)
+            .then(res => res.json())
+            .then(data => this.blog = data)
+            .catch(err => console.log(err.message))
+        }
+}
 </script>
 <template>
-    <h1 class="text-3xl font-medium tracking-tight text-white sm:text-4xl">Blog Details Page</h1>
-    <!-- <p>The blog id is {{ $route.params.id }}</p> -->
-    <p class="text-md text-white mt-5">The blog id is {{ id }}</p>
+    <div v-if="blog">
+        <h1 class="text-3xl font-medium tracking-tight text-white sm:text-4xl">{{ blog.title }}</h1>
+        <p class="text-md text-white mt-5">{{ blog.details }}</p>
+    </div>
+    <div v-else>
+        <p class="text-md text-white">Loading blog details...</p>
+    </div>
 </template>
 <style>
 </style>
