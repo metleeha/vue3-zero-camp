b983cd67f5db8023f0f19cd85c62c22c5334b4b9
Hadong Lee <hadongme@gmail.com>
2023-02-09 02:47:03 +0900

    Reusable Functions

---
+++ myblog/src/views/PostView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div v-if="error">{{ error }}</div>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }}</h3>
+    <p class="pre">{{ post.body }}</p>
+  </div>
+  <div v-else>Loading...</div>
+</template>
+
+<script>
+import getPost from '../utils/getPost'
+
+export default {
+  props: ['id'],
+  setup(props) {
+    const { error, post, load } = getPost(props.id)
+    load()
+    return { error, post }
+  },
+}
+</script>
+
+<style scoped>
+.post {
+  text-align: left;
+  padding: 5rem;
+  margin-left: 2rem;
+}
+.post h3 {
+  font-family: Merriweather, serif;
+  font-size: 1.5rem;
+  margin: 2rem 0;
+}
+.post p {
+  font-family: Raleway, sans-serif;
+  font-size: 1rem;
+  margin: 2rem 0;
+}
+.pre {
+  white-space: pre-wrap;
+}
+</style>
