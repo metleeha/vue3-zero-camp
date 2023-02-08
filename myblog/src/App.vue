d8bdda56972966fc38bc5270fa388ef79f0dbece
Hadong Lee <hadongme@gmail.com>
2023-02-09 01:46:00 +0900

    Using Props in Setup Function

--- myblog/src/App.vue
+++ myblog/src/App.vue
@@ -1,16 +1,15 @@
 <script setup>
 import { RouterView } from "vue-router";
 import ZeroFooter from "./components/ZeroFooter.vue";
-import ZeroHeader from './components/ZeroHeader.vue';
+import ZeroHeader from "./components/ZeroHeader.vue";
 </script>
 
 <template>
   <ZeroHeader />
   <main class="container mx-auto">
-    <RouterView/>
+    <RouterView />
   </main>
-  <ZeroFooter/>
+  <ZeroFooter />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- myblog/src/components/ZeroFooter.vue
+++ myblog/src/components/ZeroFooter.vue
@@ -1,7 +1,10 @@
 <template>
   <footer class="fixed bottom-0 right-0 p-4">
-    <span class="mx-auto text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023
-      <a href="https://github.com/metleeha" class="hover:underline">HadongLee</a>. All Rights Reserved.
+    <span
+      class="mx-auto text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >© 2023
+      <a href="https://github.com/metleeha" class="hover:underline">HadongLee</a
+      >. All Rights Reserved.
     </span>
   </footer>
 </template>
